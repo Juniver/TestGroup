7bb8a03994534fa988a4de8c71a2db7cf5a48499
juniver <juniversfis@gmail.com>
2014-08-20 00:43:16 +0900

    - start

---
+++ WinformConsole/WinformConsole/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformConsole
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("test1234");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("test1234");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("test1234");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("test1234");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("test1234");
+        }
+    }
+}
---
+++ WinformConsole/WinformConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FormWithConsole;
+
+namespace WinformConsole
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+#if DEBUG
+            NativeMethods.AllocConsole();
+            Console.WriteLine("Debug Console");
+#endif
+            Application.Run(new Form1());
+#if DEBUG
+            NativeMethods.FreeConsole();
+#endif
+        }
+    }
+}
